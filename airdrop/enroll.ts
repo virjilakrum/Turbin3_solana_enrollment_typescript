16a68fe98dacd4d2db70ba4766c44955f9b4b04a
Virjilakrum <asesenep15@gmail.com>
2024-07-24 16:50:39 +0300

    finish enroll

--- airdrop/enroll.ts
+++ airdrop/enroll.ts
@@ -4,17 +4,16 @@ import { IDL, WbaPrereq } from "./programs/wba_prereq";
 import wallet from "./dev-wallet.json";
 
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+
 const connection = new Connection("https://api.devnet.solana.com");
-const github = Buffer.from("<virjilakrum>", "utf8");
+
+const github = Buffer.from("virjilakrum", "utf8");
+
 const provider = new AnchorProvider(connection, new Wallet(keypair), {
   commitment: "confirmed",
 });
 
-const program = new Program<WbaPrereq>(
-  IDL,
-  new PublicKey(IDL.address),
-  provider,
-);
+const program: Program<WbaPrereq> = new Program(IDL, provider);
 
 const enrollment_seeds = [Buffer.from("prereq"), keypair.publicKey.toBuffer()];
 const [enrollment_key, _bump] = PublicKey.findProgramAddressSync(
@@ -28,13 +27,11 @@ const [enrollment_key, _bump] = PublicKey.findProgramAddressSync(
       .complete(github)
       .accounts({
         signer: keypair.publicKey,
-        prereq: enrollment_key,
-        systemProgram: PublicKey.default,
       })
       .signers([keypair])
       .rpc();
     console.log(`Success! Check out your TX here:
-https://explorer.solana.com/tx/${txhash}?cluster=devnet`);
+  https://explorer.solana.com/tx/${txhash}?cluster=devnet`);
   } catch (e) {
     console.error(`Oops, something went wrong: ${e}`);
   }
--- airdrop/programs/wba_prereq.ts
+++ airdrop/programs/wba_prereq.ts
@@ -1,58 +1,109 @@
 export type WbaPrereq = {
-  version: "0.1.0";
-  name: "wba_prereq";
-  instructions: Array<{
-    name: string;
-    discriminator: number[];
-    accounts: Array<{
-      name: string;
-      writable: boolean;
-      signer: boolean;
-      pda?: {
-        seeds: Array<{
-          kind: string;
-          value?: number[];
-          path?: string;
-        }>;
-      };
-      address?: string;
-    }>;
-    args: Array<{
-      name: string;
-      type: { defined: string };
-    }>;
-  }>;
-  accounts: Array<{
-    name: string;
-    discriminator: number[];
-  }>;
-  errors: Array<{
-    code: number;
-    name: string;
-    msg: string;
-  }>;
-  types: Array<{
-    name: string;
-    type: {
-      kind: string;
-      fields: Array<{
-        name: string;
-        type: { defined: string };
-      }>;
-    };
-  }>;
-  address: string;
+  address: "WBA52hW35HZU5R2swG57oehbN2fTr7nNhNDgfjnqUoZ";
   metadata: {
-    name: string;
-    version: string;
-    spec: string;
-    description: string;
+    name: "wba_prereq";
+    version: "0.1.0";
+    spec: "0.1.0";
+    description: "Created with Anchor";
   };
+  instructions: [
+    {
+      name: "complete";
+      discriminator: [0, 77, 224, 147, 136, 25, 88, 76];
+      accounts: [
+        {
+          name: "signer";
+          writable: true;
+          signer: true;
+        },
+        {
+          name: "prereq";
+          writable: true;
+          pda: {
+            seeds: [
+              {
+                kind: "const";
+                value: [112, 114, 101, 114, 101, 113];
+              },
+              {
+                kind: "account";
+                path: "signer";
+              },
+            ];
+          };
+        },
+        {
+          name: "system_program";
+          address: "11111111111111111111111111111111";
+        },
+      ];
+      args: [
+        {
+          name: "github";
+          type: "bytes";
+        },
+      ];
+    },
+    {
+      name: "update";
+      discriminator: [219, 200, 88, 176, 158, 63, 253, 127];
+      accounts: [
+        {
+          name: "signer";
+          writable: true;
+          signer: true;
+        },
+        {
+          name: "prereq";
+          writable: true;
+        },
+        {
+          name: "system_program";
+          address: "11111111111111111111111111111111";
+        },
+      ];
+      args: [
+        {
+          name: "github";
+          type: "bytes";
+        },
+      ];
+    },
+  ];
+  accounts: [
+    {
+      name: "Q2Prereq2024";
+      discriminator: [210, 203, 168, 103, 251, 233, 204, 6];
+    },
+  ];
+  errors: [
+    {
+      code: 6000;
+      name: "InvalidGithubAccount";
+      msg: "Invalid Github account";
+    },
+  ];
+  types: [
+    {
+      name: "Q2Prereq2024";
+      type: {
+        kind: "struct";
+        fields: [
+          {
+            name: "github";
+            type: "bytes";
+          },
+          {
+            name: "key";
+            type: "pubkey";
+          },
+        ];
+      };
+    },
+  ];
 };
 
 export const IDL: WbaPrereq = {
-  version: "0.1.0",
-  name: "wba_prereq",
   address: "WBA52hW35HZU5R2swG57oehbN2fTr7nNhNDgfjnqUoZ",
   metadata: {
     name: "wba_prereq",
@@ -94,7 +145,7 @@ export const IDL: WbaPrereq = {
       args: [
         {
           name: "github",
-          type: { defined: "bytes" },
+          type: "bytes",
         },
       ],
     },
@@ -119,7 +170,7 @@ export const IDL: WbaPrereq = {
       args: [
         {
           name: "github",
-          type: { defined: "bytes" },
+          type: "bytes",
         },
       ],
     },
@@ -145,11 +196,11 @@ export const IDL: WbaPrereq = {
         fields: [
           {
             name: "github",
-            type: { defined: "bytes" },
+            type: "bytes",
           },
           {
             name: "key",
-            type: { defined: "pubkey" },
+            type: "pubkey",
           },
         ],
       },
