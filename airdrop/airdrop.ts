317c7af69001e395bd1fe0fd674557e2ffea9000
Virjilakrum <asesenep15@gmail.com>
2024-07-24 16:02:11 +0300

    airdrop fix for [SolanaJSONRPCError]

--- airdrop/airdrop.ts
+++ airdrop/airdrop.ts
@@ -2,10 +2,15 @@ import { Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
 import wallet from "./dev-wallet.json";
 
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
-const connection = new Connection("https://api.devnet.solana.com");
+const connection = new Connection("https://api.devnet.solana.com", "confirmed");
 
 (async () => {
   try {
+    // Cüzdanın bakiyesini kontrol et
+    const balance = await connection.getBalance(keypair.publicKey);
+    console.log(`Cüzdan Bakiyesi: ${balance / LAMPORTS_PER_SOL} SOL`);
+
+    // Airdrop isteği gönder
     const txhash = await connection.requestAirdrop(
       keypair.publicKey,
       2 * LAMPORTS_PER_SOL,
