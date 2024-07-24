63be04b49fff1d4cd4759411c71687a688c99559
Virjilakrum <asesenep15@gmail.com>
2024-07-24 16:03:27 +0300

    Transfer Transaction (transfer.ts)

---
+++ airdrop/transfer.ts
@@ -0,0 +1,39 @@
+import {
+  Transaction,
+  SystemProgram,
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  sendAndConfirmTransaction,
+  PublicKey,
+} from "@solana/web3.js";
+import wallet from "./dev-wallet.json";
+
+const from = Keypair.fromSecretKey(new Uint8Array(wallet));
+const to = new PublicKey("İkinci cüzdanın public key'ini buraya yazın");
+
+const connection = new Connection("https://api.devnet.solana.com");
+
+(async () => {
+  try {
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: from.publicKey,
+        toPubkey: to,
+        lamports: LAMPORTS_PER_SOL / 100,
+      }),
+    );
+    transaction.recentBlockhash = (
+      await connection.getLatestBlockhash("confirmed")
+    ).blockhash;
+    transaction.feePayer = from.publicKey;
+
+    const signature = await sendAndConfirmTransaction(connection, transaction, [
+      from,
+    ]);
+    console.log(`Başarılı! İşlemi buradan kontrol edebilirsiniz:
+        https://explorer.solana.com/tx/${signature}?cluster=devnet`);
+  } catch (e) {
+    console.error(`Hata oluştu: ${e}`);
+  }
+})();
